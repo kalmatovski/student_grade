588d62bd93437f1beb5f0ecb3024c0738c273236
kalmatovski <jnshbkvkbr@gmail.com>
2024-05-06 15:08:29 +0600

    display admin password

--- app/login/admin/page.jsx
+++ app/login/admin/page.jsx
@@ -44,7 +44,8 @@ const AdminPanel = () => {
       <form className="w-full max-w-md" onSubmit={handleSubmit}>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="adminKey">
-            Admin Password
+            Admin Password is <b>ait23</b>
+
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
